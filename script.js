3363d352485e9ac63bf5f2473b190e20bf1b75c2
Chirag Gilhotra <chiraggilhotrain@gmail.com>
2023-02-12 14:58:47 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -1,57 +1,52 @@
-document.getElementById("ground").style.width = window.innerWidth - window.innerWidth % 50 + "px";
-document.getElementById("ground").style.height = window.innerHeight - window.innerHeight % 50 - 50 + "px";
+{
+    document.getElementById("ground").style.width = window.innerWidth - window.innerWidth % 50 + "px";
+    document.getElementById("ground").style.height = window.innerHeight - window.innerHeight % 50 - 50 + "px";
 
-score = 0;
-correct = 0;
-wrong = 0;
-per_move = 1000;
+    let score = 0;
+    let correct = 0;
+    let wrong = 0;
+    let per_move = 1000;
 
-document.addEventListener("keypress", function(event){
-    key = document.querySelector(`div.keys[char='${event.key}']`);
-    if(key == undefined)
-    {
-        wrong++;
-        score--;
+    document.addEventListener("keypress", function (event) {
+        key = document.querySelector(`div.keys[char='${event.key}']`);
+        if (key == undefined) {
+            wrong++;
+            score--;
+        }
+        else {
+            score++;
+            correct++;
+            key.remove();
+            if (score % 10 == 0) {
+                per_move -= per_move * 1 / 100;
+            }
+        }
+        document.getElementById("score").innerHTML = score;
+    });
+
+    characters = "abcdefghijklmnopqrstuvwxyz";
+    function generate_key() {
+        char = characters[Math.floor(Math.random() * characters.length)];
+        left = Math.floor(Math.random() * (((window.innerWidth - window.innerWidth % 50 - 50) / 50) + 1)) * 50;
+        document.getElementById("ground").innerHTML += `<div class='keys' style="top:0px; left:${left}px;" char="${char}">${char}</div>`;
     }
-    else
-    {
-        score++;
-        correct++;
-        key.remove();
-        if(score % 10 == 0)
-        {
-            per_move -= per_move*1/100;
+
+    function move_down() {
+        keys = document.getElementsByClassName("keys");
+        for (i = 0; i < keys.length; i++) {
+            keys[i].style.top = parseInt(keys[i].style.top.replace("px", "")) + 50 + "px";
+            if ((window.innerHeight - window.innerHeight % 50 - 100) <= parseInt(keys[i].style.top.replace("px", ""))) {
+                clearInterval(move_down_interval);
+                clearInterval(generate_key_interval);
+                document.getElementById("scorediv").innerText = `Gameover. Score = ${score}, Correct = ${correct}, Wrong = ${wrong}.`;
+            }
         }
     }
-    document.getElementById("score").innerHTML = score;
-});
 
-characters = "abcdefghijklmnopqrstuvwxyz";
-function generate_key()
-{
-    char = characters[Math.floor(Math.random()*characters.length)];
-    left = Math.floor(Math.random()*(((window.innerWidth - window.innerWidth%50 - 50)/50)+1))*50;
-    document.getElementById("ground").innerHTML += `<div class='keys' style="top:0px; left:${left}px;" char="${char}">${char}</div>`;
+
+    document.getElementById("start").addEventListener("click", function () {
+        move_down_interval = setInterval(move_down, per_move);
+        generate_key_interval = setInterval(generate_key, per_move);
+        document.getElementById("start").style.display = "none";
+    });
 }
-
-function move_down()
-{
-    keys = document.getElementsByClassName("keys");
-    for(i=0;i<keys.length;i++)
-    {
-        keys[i].style.top = parseInt(keys[i].style.top.replace("px","")) + 50 + "px";
-        if((window.innerHeight - window.innerHeight%50 - 100) <= parseInt(keys[i].style.top.replace("px","")))
-        {
-            clearInterval(move_down_interval);
-            clearInterval(generate_key_interval);
-            document.getElementById("scorediv").innerText = `Gameover. Score = ${score}, Correct = ${correct}, Wrong = ${wrong}.`;
-        }
-    }
-}
-
-
-document.getElementById("start").addEventListener("click", function(){
-    move_down_interval = setInterval(move_down, per_move);
-    generate_key_interval = setInterval(generate_key, per_move);
-    document.getElementById("start").style.display = "none";
-});
